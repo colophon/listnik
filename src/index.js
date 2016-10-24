1b3715166f6625a5d17430246db25119dac591a3
Ruhi Bloodworth <ruhi.bloodworth@gmail.com>
2016-10-24 13:10:39 -0500

    persist the store to browser local storage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { compose, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import persistState, {mergePersistedState} from 'redux-localstorage'
+import adapter from 'redux-localstorage/lib/adapters/localStorage'
 import App from './containers/App'
-import reducer from './reducers'
+import rootReducer from './reducers'
 
 import 'react-mdl/extra/material.css'
 import 'react-mdl/extra/material.js'
 
-const store = createStore(reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const reducer = compose(
+  mergePersistedState()
+)(rootReducer)
+
+const storage = (adapter(window.localStorage))
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const enhancer = composeEnhancers(
+  persistState(storage, 'lisnik')
+)
+
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
